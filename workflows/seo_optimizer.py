66e6f8e471bbe23f76b850c3e40f784e30307b26
talalnewjersey-ops <talalnewjersey@gmail.com>
2026-03-31 21:12:10 -0400

    Update seo_optimizer.py

--- workflows/seo_optimizer.py
+++ workflows/seo_optimizer.py
@@ -1,10 +1,17 @@
 import requests
 
-url = "https://moneyabroadguide.com/wp-json/mag/v1/posts"
+GET_URL = "https://moneyabroadguide.com/wp-json/mag/v1/posts"
+UPDATE_URL = "https://moneyabroadguide.com/wp-json/mag/v1/update-post"
 
-response = requests.get(url)
-
-posts = response.json()
+posts = requests.get(GET_URL).json()
 
 for post in posts:
-    print("Optimizing:", post["title"])
+    old_title = post["title"]
+    new_title = old_title + " (2026 Guide)"
+
+    print("Updating:", old_title)
+
+    requests.post(UPDATE_URL, json={
+        "id": post["id"],
+        "title": new_title
+    })
